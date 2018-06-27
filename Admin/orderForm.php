5eed6a3c02d237814342b9ec7745e090cbf7e21f
verysuper <verysuper@live.com>
2018-06-27 20:55:20 +0800

    0627_6

---
+++ Admin/orderForm.php
@@ -0,0 +1,223 @@
+<?php require_once('../Connections/shop.php'); ?>
+<?php
+if (!function_exists("GetSQLValueString")) {
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  if (PHP_VERSION < 6) {
+    $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+  }
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? doubleval($theValue) : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+}
+
+mysql_select_db($database_shop, $shop);
+$query_ordersList = "SELECT * FROM orders ORDER BY orderDate DESC";
+$ordersList = mysql_query($query_ordersList, $shop) or die(mysql_error());
+$row_ordersList = mysql_fetch_assoc($ordersList);
+$totalRows_ordersList = mysql_num_rows($ordersList);
+ session_start(); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/ShopAdmin.dwt.php" codeOutsideHTMLIsLocked="false" -->
+<script language="javascript" src="../Scripts/menu.js"/></script>
+<head>
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>伴手網後台</title>
+<!-- InstanceEndEditable -->
+
+<!-- Meta Tags -->
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=utf-8" />
+<meta name="robots"      content="index, follow" />
+<meta name="description" content="" />
+<meta name="keywords"    content="" />
+<meta name="author"      content="" />
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="" />
+
+<!-- CSS -->
+<link rel="stylesheet" href="../css/styles.css" media="all" type="text/css" />
+
+<!-- RSS -->
+<link rel="alternate" href="" title="RSS Feed" type="application/rss+xml" />
+<!-- InstanceBeginEditable name="head" -->
+<!-- InstanceEndEditable -->
+</head>
+<body>
+  <div id="header">
+      <div class="subContainer">
+            <div id="logo">
+            <div id="box">H2H</div>
+            <p>咱的好伴手後台</p>
+            </div><!-- /logo -->
+    </div><!-- /subContainer -->
+</div><!-- header -->
+    
+<div id="navigation">   
+	<ul id="sddm">
+        <li><a href="index.php">首頁</a></li>
+        <li><a href="productBrowser.php">商品瀏覽</a></li>        
+	    <li><a href="#" 
+        onmouseover="mopen('m1')" 
+        onmouseout="mclosetime()">商品管理</a>
+        <div id="m1" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="productState.php">商品上下架</a>
+        <a href="productAdd.php">商品新增</a>
+        </div>
+    	</li>        
+        <li><a href="orderForm.php">訂單處理</a></li>   
+	    <li><a href="#" 
+        onmouseover="mopen('m2')" 
+        onmouseout="mclosetime()">公佈欄管理</a>
+        <div id="m2" 
+            onmouseover="mcancelclosetime()" 
+            onmouseout="mclosetime()">
+        <a href="publishBrowser.php">訊息瀏覽、維護</a>
+        <a href="publishAdd.php">訊息新增</a>
+        </div>
+    	</li>        
+		</ul> 
+<div style="clear:both"></div>    
+</div><!-- /navigation -->
+<object id="FlashID" classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="780" height="120">
+    <param name="movie" value="../Scripts/pic.swf" />
+    <param name="quality" value="high" />
+    <param name="wmode" value="opaque" />
+    <param name="swfversion" value="8.0.35.0" />
+
+    <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+    <!--[if !IE]>-->
+    <object type="application/x-shockwave-flash" data="../Scripts/pic.swf" width="780" height="120">
+      <!--<![endif]-->
+      <param name="quality" value="high" />
+      <param name="wmode" value="opaque" />
+      <param name="swfversion" value="8.0.35.0" />
+      <param name="expressinstall" value="../Scripts/expressInstall.swf" />
+      <div>
+        <h4>這個頁面上的內容需要較新版本的 Adobe Flash Player。</h4>
+        <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="取得 Adobe Flash Player" width="112" height="33" /></a></p>
+      </div>
+      <!--[if !IE]>-->
+    </object>
+    <!--<![endif]-->
+  </object>
+<div id="container"><!-- InstanceBeginEditable name="EditRegion1" -->
+  <div id="primaryContent">
+    <h2>會員訂單瀏覽</h2>
+    <h3>依訂單日期遞減排序，出貨處理或查看訂單細目請按下「出貨」按鈕。</h3>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <?php if ($totalRows_ordersList > 0) { // Show if recordset not empty ?>
+  <table>
+    <tr>
+      <th align="center"><p>訂單編號</p></th>
+      <th align="center"><p>會員ID</p></th>
+      <th align="center"><p>訂購日期</p></th>
+      <th align="center"><p>出貨否</p></th>
+      <th align="center"><p>出貨/詳細</p></th>
+    </tr>
+    <?php do { ?><form id="form1" method="POST" action="">    
+      <tr>
+        <td align="center"><p><?php echo $row_ordersList['O_id']; ?></p></td>
+        <td align="center"><p><?php echo $row_ordersList['userid']; ?></p></td>
+        <td align="center"><h4><?php echo $row_ordersList['orderDate']; ?></h4></td>
+        <td align="center"><h5><?php echo $row_ordersList['shipment']; ?></h5></td>
+        <td align="center"><button type="submit">出貨/詳細</button></td>
+      </tr>
+    </form> <?php } while ($row_ordersList = mysql_fetch_assoc($ordersList)); ?>
+  </table>
+  <?php } // Show if recordset not empty ?>
+  <?php if ($totalRows_ordersList == 0) { // Show if recordset empty ?>
+  <h5>目前尚無訂單 T_T</h5>
+  <?php } // Show if recordset empty ?>
+<p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <!-- /comments -->
+  </div>
+<!-- InstanceEndEditable -->
+  <!-- /primaryContent -->
+  <div id="secondaryContent">
+    <p>&nbsp;</p>
+    <h3>未處理訂單</h3>
+    <h3>相關連結</h3>
+    <!-- 
+            This part was designed to handle images
+            -->
+    <ul class="links">
+      <li><a href="http://www.pcschool.com.tw/">巨匠電腦</a></li>
+      <li><a href="http://kid.pcschool.com.tw/">兒童電腦</a></li>      
+      <li><a href="http://www.soeasyedu.com.tw/">數位學習</a></li>
+      <li><a href="http://www.soeasyedu.com.tw/">巨匠美語</a></li>  
+      <li><a href="http://www.off.com.tw/weiblog">給我建議</a>
+    </ul>
+    <?php if(!isset($_SESSION['MM_Username'])){?>
+    <h3>管理員登入</h3>
+    <form action="loginCheck.php" method="post">
+      <p>
+        <input value="user" name="user" placeholder="user" />
+      </p>
+      <p>
+        <input name="password" type="password" value="" placeholder="password" />
+      </p>
+      <p>
+        <button type="submit">送出</button>
+      </p>
+    </form>
+    <?php }else{ ?>
+    <h3>管理員服務</h3>
+    <form action="logout.php" method="post">
+    <p>歡迎 <?php echo $_SESSION['MM_Username'];?> 您光臨</p>
+    <p><button type="submit">登出</button></p>
+    </form>
+	<?php }?>
+  </div>
+  <!-- /secondaryContent -->
+        <br class="clear" />    
+</div><!-- container -->
+    
+    <div id="footer">
+        <ul>
+            <li id="copyright">&copy; 2099 Free for Gjun student member</li>
+            <li id="links">
+                <ul>
+                    <li><a href="#">XHTML</a> |</li>
+                    <li><a href="#">CSS</a> |</li>
+                    <li><a href="http://gjun.tw">ALAN CHEN</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div><!-- /footer -->
+</body>
+<!-- InstanceEnd --></html>
+<?php
+mysql_free_result($ordersList);
+?>
