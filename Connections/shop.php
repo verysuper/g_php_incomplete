9b686c4937e780202af78b2b439a7763bb81db32
verysuper <verysuper@live.com>
2018-07-10 22:55:39 +0800

    aaa

--- Connections/shop.php
+++ Connections/shop.php
@@ -5,7 +5,7 @@
 $hostname_shop = "localhost";
 $database_shop = "shop";
 $username_shop = "root";
-$password_shop = "00000000";
+$password_shop = "";
 $shop = mysql_pconnect($hostname_shop, $username_shop, $password_shop) or trigger_error(mysql_error(),E_USER_ERROR);
 mysql_query("set names 'utf8'");
 date_default_timezone_set("Asia/Taipei");
